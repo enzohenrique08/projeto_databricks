496964b2e0236eb930a3ba128cb655eb560f3bdd
enzohenrique08 <enzohenrique17jf@gmail.com>
2024-07-30 17:37:13 -0300

    Update ingestao.py

--- src/ingestao.py
+++ src/ingestao.py
@@ -13,16 +13,3 @@ now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 path = f"/Volumes/raw/pokemon/pokemon_raw/pokemon_list/{now}.json"
 with open(path, 'w') as open_file:
     json.dump(data_save, open_file)import requests
-import json 
-import datetime
-
-
-url = 'https://pokeapi.co/api/v2/pokemon?limit=2000'
-resp = requests.get(url)
-data = resp.json()
-
-data_save = data['results']
-now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-path = f"/Volumes/raw/pokemon/pokemon_raw/pokemon_list/{now}.json"
-with open(path, 'w') as open_file:
-    json.dump(data_save, open_file)
